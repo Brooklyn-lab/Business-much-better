1171803f445737b98596eca929e8ecab880a8a40
Brooklyn-lab <61649133+Brooklyn-lab@users.noreply.github.com>
2020-10-30 14:18:52 +0200

    add css

--- js/main.js
+++ js/main.js
@@ -1,78 +1,84 @@
 jQuery(document).ready(function () {
 
-    // //-------------footer-date------------------------
-    // let date = new Date().getFullYear();
-    // $("#date").text(new Date().getFullYear());
+    let burger = $(".header__burger");
 
-    // // Filter
-    // let invisible = $(".filter_invisible");
-    // let more = $(".filter__more");
-    // let btns = $(".tabs__btn");
+    burger.on("click", function() {
+        $(".nav__wrapper").toggleClass("menu");
+    })
 
-    // //-------------------Кнопка "More"------------------
-    // invisible.hide();
+    //-------------footer-date------------------------
+    let date = new Date().getFullYear();
+    $("#date").text(new Date().getFullYear());
 
-    // more.on("click", function () {
-    //     invisible.toggle();
-    // });
+    // Filter
+    let invisible = $(".filter_invisible");
+    let more = $(".filter__more");
+    let btns = $(".tabs__btn");
 
-    // //--------------------Категории---------------------
-    // btns.on("click", function () {
-    //     $(this)
-    //         .addClass("active")
-    //         .siblings()
-    //         .removeClass("active");
-    //     let cards = $(".tabs__card");
-    //     cards.removeClass("active");
-    //     let btnText = $(this)
-    //         .text()
-    //         .toLowerCase();
-    //     let final = cards.filter(function () {
-    //         if (btnText === "all") {
-    //             return cards;
-    //         } else {
-    //             return $(this).data("category") === btnText;
-    //         }
-    //     });
-    //     final.each(function () {
-    //         $(this).addClass("active");
-    //     });
-    // });
+    //-------------------Кнопка "More"------------------
+    invisible.hide();
 
-    // let blogBtn = $('#blog-btn');
-    // let postInvisibile = $('.post__link_invisible');
-    // let recentPosts = $('.recent-posts__items');
-    // let tags = $('.tags__body');
-    // let instagram = $('.instagram__body');
-    // let recentPostsWrapper = $('.recent-posts__wrapper');
-    // let tagsWrapper = $('.tags__wrapper');
-    // let instaWrapper = $('.instagram__wrapper');
+    more.on("click", function () {
+        invisible.toggle();
+    });
 
-    // $('.sidebar-title::before').css('display: none');
+    //--------------------Категории---------------------
+    btns.on("click", function () {
+        $(this)
+            .addClass("active")
+            .siblings()
+            .removeClass("active");
+        let cards = $(".tabs__card");
+        cards.removeClass("active");
+        let btnText = $(this)
+            .text()
+            .toLowerCase();
+        let final = cards.filter(function () {
+            if (btnText === "all") {
+                return cards;
+            } else {
+                return $(this).data("category") === btnText;
+            }
+        });
+        final.each(function () {
+            $(this).addClass("active");
+        });
+    });
 
-    // postInvisibile.hide();
-    // recentPosts.show();
-    // tags.show();
-    // instagram.show();
+    let blogBtn = $('#blog-btn');
+    let postInvisibile = $('.post__link_invisible');
+    let recentPosts = $('.recent-posts__items');
+    let tags = $('.tags__body');
+    let instagram = $('.instagram__body');
+    let recentPostsWrapper = $('.recent-posts__wrapper');
+    let tagsWrapper = $('.tags__wrapper');
+    let instaWrapper = $('.instagram__wrapper');
 
-    // blogBtn.on("click", function () {
-    //     postInvisibile.toggle();
-    // });
+    $('.sidebar-title::before').css('display: none');
 
-    // recentPostsWrapper.on("click", function () {
-    //     recentPosts.toggle();
-    //     recentPostsWrapper.toggleClass('posts__wrapper_active ');
-    // });
+    postInvisibile.hide();
+    recentPosts.show();
+    tags.show();
+    instagram.show();
 
-    // tagsWrapper.on("click", function () {
-    //     tags.toggle();
-    //     tagsWrapper.toggleClass('posts__wrapper_active ');
-    // });
+    blogBtn.on("click", function () {
+        postInvisibile.toggle();
+    });
 
-    // instaWrapper.on("click", function () {
-    //     instagram.toggle();
-    //     instaWrapper.toggleClass('posts__wrapper_active ');
-    // });
+    recentPostsWrapper.on("click", function () {
+        recentPosts.toggle();
+        recentPostsWrapper.toggleClass('posts__wrapper_active ');
+    });
+
+    tagsWrapper.on("click", function () {
+        tags.toggle();
+        tagsWrapper.toggleClass('posts__wrapper_active ');
+    });
+
+    instaWrapper.on("click", function () {
+        instagram.toggle();
+        instaWrapper.toggleClass('posts__wrapper_active ');
+    });
 
 
     // let a = 10;
@@ -231,6 +237,6 @@ jQuery(document).ready(function () {
     //         console.log(arr[i]);
     //     }
     // }
-    let arr = [2, 5, -9, 15, 0, -4];
+    // let arr = [2, 5, -9, 15, 0, -4];
     
 });
